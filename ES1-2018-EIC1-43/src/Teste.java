49bb9db254248ae05792da3efb65ff896a1df8ec
fmsan <fmsan@172.17.8.97>
2018-10-24 17:42:49 +0100

    Adicionada App + Twitter.

--- ES1-2018-EIC1-43/src/Teste.java
+++ ES1-2018-EIC1-43/src/Teste.java
@@ -1,8 +1,7 @@
-
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println("Teste");
+		new App(new Gui(), new Facebook(), new Twitter());
 
 	}
 
--- README.md
+++
@@ -1,5 +0,0 @@
-Grupo 43;
-Eduardo Martins nº 73190;
-Filipe Santana nº 73796;
-Gonçalo Fernandes nº 73538;
-Guilherme Relvas nº 64909;
