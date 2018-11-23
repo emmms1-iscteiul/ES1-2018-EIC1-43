a1ebd58c11dbfca4667f993be1429873acce8b4b
fmsan <fmsan@172.17.9.189>
2018-11-23 11:44:31 +0000

    Testes twitter e e-mail com cobertura > 90%

--- ES1-2018-EIC1-43/src/Teste.java
+++
@@ -1,8 +0,0 @@
-public class Teste {
-
-	public static void main(String[] args) {
-		new App(new Gui(), new Facebook(), new Twitter());
-
-	}
-
-}
---
+++ ES1-2018-EIC1-43/src/test/java/EIC1_43/BDA/AllTests.java
@@ -0,0 +1,16 @@
+package EIC1_43.BDA;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	EmailTest.class,
+	TwitterTest.class,
+})
+public class AllTests {
+	
+}
+
+
